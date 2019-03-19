64c14681dd163f1f243a38f776c4dfed685514c5
Corey <corey.zhu@sap.com>
2019-03-19 09:58:53 +0800

    update_0319

--- src/net/atpco/java/training/homework/round4/SourceCodeCalculator.java
+++ src/net/atpco/java/training/homework/round4/SourceCodeCalculator.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.Arrays;
 
 public class SourceCodeCalculator {
     public static void main(String[] args) {
@@ -12,12 +13,12 @@ public class SourceCodeCalculator {
     }
 
     private static void WinFrame() {
-        JFrame winFrame = new JFrame("SourceCodeCalculator");
+        final JFrame winFrame = new JFrame("SourceCodeCalculator");
         final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        final int frameWidth = 400;                     // 主窗口宽度
-        final int frameHeight = 500;                    // 主窗口高度
-        final int basicWidth = frameHeight / 20;        // 20
-        final int basicHeight = frameHeight / 20;       // 25
+        final int frameWidth = 400;                         // 主窗口宽度
+        final int frameHeight = 500;                        // 主窗口高度
+        final int basicWidth = frameHeight / 20;            // 20
+        final int basicHeight = frameHeight / 20;           // 25
         final JTextField filePath = new JTextField();
         final JButton fileChoose = new JButton("Select");
         final JButton fileCalculate = new JButton("Calculate");
@@ -26,6 +27,10 @@ public class SourceCodeCalculator {
         final JCheckBox JCB_CC = new JCheckBox(" C# ");
         final JCheckBox JCB_Python = new JCheckBox(" py ");
         final JTextArea calculateResult = new JTextArea();
+        final int totalRows;
+        final int emptyRows;
+        final int annotationRows;
+        final int codingRows;
 
         // 框体设置
         winFrame.setSize(frameWidth, frameHeight);
@@ -33,7 +38,6 @@ public class SourceCodeCalculator {
         winFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         winFrame.setLayout(null);
         winFrame.setResizable(false);
-
 
         // 界面元素设计
         filePath.setBounds(basicWidth, basicHeight, basicWidth * 5, basicHeight);
@@ -59,7 +63,8 @@ public class SourceCodeCalculator {
 
         calculateResult.setBounds(basicWidth * 2, basicHeight * 5, basicWidth * 12, basicHeight * 13);
         calculateResult.setFont(new Font("Microsoft YaHei", Font.PLAIN, 18));
-        calculateResult.setEditable(false);
+        calculateResult.setEditable(false);         // 不可编辑
+        calculateResult.setLineWrap(true);          // 自动换行
 
         winFrame.add(filePath);
         winFrame.add(fileChoose);
@@ -71,18 +76,36 @@ public class SourceCodeCalculator {
         winFrame.add(calculateResult);
         winFrame.setVisible(true);
 
-        // 监听
+        // 文件选择器监听
         fileChoose.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JFileChooser jfc = new JFileChooser();
+                String showMessage = "";
                 jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+                jfc.setMultiSelectionEnabled(true);
                 jfc.showDialog(new Label(), "Choose files");
-                File file = jfc.getSelectedFile();
-                if (file.isDirectory()) {
-                    System.out.println("文件夹:" + file.getAbsolutePath());
-                } else if(file.isFile()) {
-                    System.out.println("文件:" + file.getAbsolutePath());
+                File[] files = jfc.getSelectedFiles();
+                for (File file : files) {
+                    if (file.isDirectory()) {
+                        // 文件夹的处理
+                        calculateResult.setText("文件夹 : " + file.getAbsolutePath());
+                    } else if(file.isFile()) {
+                        // 单个文件的处理
+                        // 截取文件名
+                        String fileName = file.getAbsolutePath().substring(file.getAbsolutePath().lastIndexOf("\\") + 1);
+                        // 截取文件后缀
+                        String[] strArray = fileName.split("\\.");
+                        int suffixIndex = strArray.length - 1;
+                        String fileType = strArray[suffixIndex];
+                        String[] fileTypes = {"java", "c", "cs", "py"};
+                        if (Arrays.asList(fileTypes).contains(fileType)) {
+                            showMessage = showMessage + "文件 : " + file.getAbsolutePath();
+                            calculateResult.setText(showMessage);
+                        } else {
+                            JOptionPane.showMessageDialog(null, "该文件不是Java、C、C#、Python的代码文件，请重新选择", " 警 告 ： ", JOptionPane.WARNING_MESSAGE);
+                        }
+                    }
                 }
             }
         });
